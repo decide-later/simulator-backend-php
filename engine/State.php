a3bb4317dc33325445643d57e227528a3787b2e3
Alexander Makarov <sam@rmcreative.ru>
2019-04-21 00:10:24 +0300

    Added state constants, fixed script

--- engine/State.php
+++ engine/State.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace app\engine;
-
 
 class State
 {
--- scripts/travel.php
+++ scripts/travel.php
@@ -3,44 +3,53 @@
 use app\engine\Flags;
 use app\engine\Script;
 use app\engine\State;
-use app\engine\Transition;
+
+class States {
+    public const START = 'start';
+    public const TOO_DANGEROUS = 'too_dangerous';
+    public const VISA = 'visa';
+    public const AIRPORT = 'airport';
+    public const LATE = 'late';
+    public const VISA_FAIL = 'visa_fail';
+    public const FLY = 'fly';
+}
 
 $script = new Script('travel', [
-    'start' => State::content('Вы хотите в путешествие. Куда поедем?')
-        ->transition('too_dangeous', 'В Дамаск', 'too_dangeous')
-        ->transition('visa_munich', 'В Мюнхен', 'visa'),
+    States::START => State::content('Вы хотите в путешествие. Куда поедем?')
+        ->transition('too_dangeous', 'В Дамаск', States::TOO_DANGEROUS)
+        ->transition('visa_munich', 'В Мюнхен', States::VISA),
 
-    'too_dangeous' => State::content('Не очень хорошая идея. Обстановка в Сирии сейчас совсем не для путешествий.')
-        ->transition('try_again', 'Попробуй ещё раз', 'start'),
+    States::TOO_DANGEROUS => State::content('Не очень хорошая идея. Обстановка в Сирии сейчас совсем не для путешествий.')
+        ->transition('try_again', 'Попробуй ещё раз', states::START),
 
-    'visa' => State::content('Едем в Мюнхен! Делаем визу?')
+    States::VISA => State::content('Едем в Мюнхен! Делаем визу?')
         ->transition('visa_yes', 'Да', function(array $fields) {
             Flags::write('visa_fail', 0);
-            return 'airport';
+            return States::AIRPORT;
         })
         ->transition('visa_no', 'Нет', function (array $fields) {
             Flags::write('visa_fail', 1);
-            return 'airport';
+            return States::AIRPORT;
         }),
 
-    'airport' => State::content('Вы в аэророту. Идём на паспортный контроль?')
+    States::AIRPORT => State::content('Вы в аэророту. Идём на паспортный контроль?')
         ->transition('border_go', 'Идём', function (array $fields) {
             $visaFail = Flags::read('visa_fail');
             if ($visaFail) {
-                return 'visa_fail';
+                return States::VISA_FAIL;
             }
 
-            return 'fly';
+            return States::FLY;
         })
         ->transition('border_late', 'Ой не', 'late'),
 
 
-    'late' => State::content('Вы опозлали на самолёт.')
-        ->transition('try_again', 'Попробуй ещё раз', 'airport'),
-    'visa_fail' => State::content('Вас не пустили. Мюнхен в ЕС, нужно было делать визу.')
-        ->transition('try_again', 'Попробуй ещё раз', ''),
-    'fly' => State::content('Полетели!')
-        ->transition('restart', 'Ещё раз?', 'start')
+    States::LATE => State::content('Вы опозлали на самолёт.')
+        ->transition('try_again', 'Попробуй ещё раз', States::AIRPORT),
+    States::VISA_FAIL => State::content('Вас не пустили. Мюнхен в ЕС, нужно было делать визу.')
+        ->transition('try_again', 'Попробуй ещё раз', States::VISA),
+    States::FLY => State::content('Полетели!')
+        ->transition('restart', 'Ещё раз?', States::START)
 ]);
 
 return $script;
