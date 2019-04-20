10e21e5dd7a97955eb604d565d70ac9975757100
Alexander Makarov <sam@rmcreative.ru>
2019-04-21 01:12:46 +0300

    Do not run in debug mode on production

--- web/index.php
+++ web/index.php
@@ -1,7 +1,9 @@
 <?php
 
+$isProduction = $_SERVER['HTTP_HOST'] === 'states-api.rmcreative.ru';
+
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_DEBUG') or define('YII_DEBUG', !$isProduction);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../vendor/autoload.php';
